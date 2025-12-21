5eb074f30dbbff7bcf6822d1c7782f3d06602b60
t0tue <totue662@gmail.com>
2025-12-22 03:24:02 +0900

    Delete secret.js

--- secret.js
+++
@@ -1,9 +0,0 @@
-export const API_KEY = {
-  apiKey: "AIzaSyByGjsbzMNtt5oP4-WQdP3GQYj17kCZZTg",
-  authDomain: "web-chat-project-5add6.firebaseapp.com",
-  projectId: "web-chat-project-5add6",
-  storageBucket: "web-chat-project-5add6.firebasestorage.app",
-  messagingSenderId: "717726064112",
-  appId: "1:717726064112:web:041d2112ada17ef9359135",
-  measurementId: "G-708GHVNSWG"
-};
