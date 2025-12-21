aa92e552070bcfe2f4ef3ffa6b484caeed60d0cf
t0tue <totue662@gmail.com>
2025-12-22 03:34:05 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,12 @@
-const API_KEY = import.meta.env.MY_KEYS.split(',');
+const API_KEY = {
+  apiKey: "AIzaSyByGjsbzMNtt5oP4-WQdP3GQYj17kCZZTg",
+  authDomain: "web-chat-project-5add6.firebaseapp.com",
+  projectId: "web-chat-project-5add6",
+  storageBucket: "web-chat-project-5add6.firebasestorage.app",
+  messagingSenderId: "717726064112",
+  appId: "1:717726064112:web:041d2112ada17ef9359135",
+  measurementId: "G-708GHVNSWG"
+};
 
 const app = firebase.initializeApp(API_KEY);
 const auth = firebase.auth(app);
