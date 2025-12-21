f8985d57c17d1b8ea8752ef0ad335248b4c190a0
t0tue <totue662@gmail.com>
2025-12-22 03:06:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-require('dotenv').config();
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECTID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-    appId: process.env.FIREBASE_APPID,
-    measurementId: process.env.FIREBASE_MEASUREMENTID
-};    
+import {API_KEY} from './secret.js';
 
-const app = firebase.initializeApp(firebaseConfig);
-        
-
+const app = firebase.initializeApp(API_KEY);
 const db = firebase.firestore(app);
 const auth = firebase.auth(app);
 const btn = document.getElementById("theme-toggle");
@@ -930,3 +919,4 @@ saveChannelBtn.addEventListener('click', async () => { // ⭐ async 키워드 �
 
 
 });
+
